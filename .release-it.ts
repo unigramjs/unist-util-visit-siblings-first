a40af1194123c5496ebbd074a8ba055f056602b9
Qz <quadratz@proton.me>
2025-08-24 07:40:08 +0700

    chore(dev): use release-it (#5)
    * chore(dev): use release-it
    
    Signed-off-by: qz <quadratz@proton.me>
    
    * chore(dev): only publish on ci
    
    Signed-off-by: qz <quadratz@proton.me>
    
    ---------
    
    Signed-off-by: qz <quadratz@proton.me>

---
+++ .release-it.ts
@@ -0,0 +1,24 @@
+import type { Config } from 'release-it'
+
+export default {
+	git: {
+		commit: true,
+		tag: true,
+		push: true,
+	},
+	github: {
+		release: true,
+		preRelease: true,
+	},
+	npm: {
+		publish: false,
+	},
+	plugins: {
+		'@release-it/conventional-changelog': {
+			preset: {
+				name: 'angular',
+			},
+			infile: 'CHANGELOG.md',
+		},
+	},
+} satisfies Config
